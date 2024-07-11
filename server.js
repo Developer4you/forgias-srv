0260c884889de8d9f6f91fe8d55db776ce888048
andrei andrei <andrei@joyspay.com>
2024-07-11 11:52:32 +0300

    refactor

--- server.js
+++ server.js
@@ -1,59 +1,59 @@
 const express = require('express');
-// const axios = require('axios');
-// const https = require('https');
-// const cors = require('cors')
+const axios = require('axios');
+const https = require('https');
+const cors = require('cors')
 
 const app = express();
 const port = process.env.PORT || 3000; // Порт, на котором будет запущен ваш сервер
-// app.use(cors({
-//     credentials:true,
-//     origin: [process.env.CLIENT_URL, 'http://localhost:5173']
-// }))
+app.use(cors({
+    credentials:true,
+    origin: [process.env.CLIENT_URL, 'http://localhost:5173']
+}))
 
 // Создаем экземпляр Axios с настройками для отправки POST запроса
-// const instance = axios.create({
-//     httpsAgent: new https.Agent({
-//         rejectUnauthorized: false, // Отключаем проверку сертификата (небезопасно!)
-//     }),
-// });
+const instance = axios.create({
+    httpsAgent: new https.Agent({
+        rejectUnauthorized: false, // Отключаем проверку сертификата (небезопасно!)
+    }),
+});
 
-// async function fetchContractData(id) {
-//     try {
-//         const response = await instance.get(`https://www.gias.by/purchase/api/v1/purchase/${id}`);
-//
-//         return response.data.oneSourcePurchase; // Возвращаем данные из запроса
-//     } catch (error) {
-//         console.error('Error fetching data:', error.message);
-//         throw error; // Пробрасываем ошибку для дальнейшей обработки
-//     }
-// }
+async function fetchContractData(id) {
+    try {
+        const response = await instance.get(`https://www.gias.by/purchase/api/v1/purchase/${id}`);
+
+        return response.data.oneSourcePurchase; // Возвращаем данные из запроса
+    } catch (error) {
+        console.error('Error fetching data:', error.message);
+        throw error; // Пробрасываем ошибку для дальнейшей обработки
+    }
+}
 
 // Функция для выполнения POST-запроса к сайту gias.by и возврата данных
-// async function fetchPurchasesData(textSearch) {
-//     try {
-//         const response = await instance.post('https://www.gias.by/search/api/v1/search/purchases', {
-//             page: 0,
-//             pageSize: 100,
-//             contextTextSearch: textSearch,
-//             sortField: 'dtCreate',
-//             sortOrder: 'DESC',
-//         });
-//         const contractsId = response.data.content
-//             .filter(e => e.stateName === 'Договор подписан')
-//             .map(e => e.purchaseGiasId);
-//
-//         // Создаем массив промисов для каждого запроса на данные по договору
-//         const contractPromises = contractsId.map(e => fetchContractData(e));
-//
-//         // Ждем завершения всех запросов с помощью Promise.all
-//         const contractData = await Promise.all(contractPromises);
-//
-//         return contractData;
-//     } catch (error) {
-//         console.error('Error fetching data:', error.message);
-//         throw error; // Пробрасываем ошибку для дальнейшей обработки
-//     }
-// }
+async function fetchPurchasesData(textSearch) {
+    try {
+        const response = await instance.post('https://www.gias.by/search/api/v1/search/purchases', {
+            page: 0,
+            pageSize: 100,
+            contextTextSearch: textSearch,
+            sortField: 'dtCreate',
+            sortOrder: 'DESC',
+        });
+        const contractsId = response.data.content
+            .filter(e => e.stateName === 'Договор подписан')
+            .map(e => e.purchaseGiasId);
+
+        // Создаем массив промисов для каждого запроса на данные по договору
+        const contractPromises = contractsId.map(e => fetchContractData(e));
+
+        // Ждем завершения всех запросов с помощью Promise.all
+        const contractData = await Promise.all(contractPromises);
+
+        return contractData;
+    } catch (error) {
+        console.error('Error fetching data:', error.message);
+        throw error; // Пробрасываем ошибку для дальнейшей обработки
+    }
+}
 
 // Обработчик GET запроса на сервере для получения данных и их отправки клиенту
 app.get('/api/getData', async (req, res) => {
@@ -65,8 +65,7 @@ app.get('/api/getData', async (req, res) => {
         }
 
         // Выполняем функцию fetchPurchasesData для получения данных
-        const dataFromGias = ['rrr','ttt','yyy'];
-        // const dataFromGias = await fetchPurchasesData(contextTextSearch);
+        const dataFromGias = await fetchPurchasesData(contextTextSearch);
         // Отправляем данные обратно клиенту
         res.json(dataFromGias);
     } catch (error) {
